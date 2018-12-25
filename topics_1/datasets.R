b8dd3978d5c6a16cae6005737da3c4b7b9d6dbbf
Dhiraj Upadhyaya <dup1966@gmail.com>
2018-12-25 15:32:39 +0530

    a

---
+++ topics_1/datasets.R
@@ -0,0 +1,7 @@
+# Data Sets
+
+#Currently Available Data Sets
+data()
+
+#Datasets in Library/ Package
+data(package='MASS')
---
+++ topics_1/valuegenerate.R
@@ -0,0 +1,32 @@
+# Value Generation using these functions
+
+#seq
+seq(1,100, by=5)
+seq(0,99, length.out=10)
+
+#rep
+rep(c(1,2,3), times=5)
+rep(c(1,2,3), each=5)
+
+#rnorm
+rnorm(n=100, mean=60, sd=10)
+
+
+#runif
+runif(n=100, min=60, max=100)
+
+#sample
+sample(1:10, size=100, replace=T)
+
+#months
+month.abb[1:10]
+#alphabets
+letters[1:10]
+
+
+#Built in Constants
+LETTERS
+letters
+month.abb
+month.name
+pi
